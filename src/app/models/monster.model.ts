6e2c180e35d1c625bdadaa3a8e7d31c5d79ae261
Loic Vanel MABOMESI <loicvanelmabomesinkanko@gmail.com>
2025-05-06 17:45:08 +0200

    ajout model

---
+++ src/app/models/monster.model.ts
@@ -0,0 +1,8 @@
+export class Monster{  
+  name: String = "L bo goss 1";
+  hp: number = 40;
+  figureCaption: String = "Num 237 monstre";
+  attackName: String = "Geo attack hahah";
+  attackStrength: number =60;
+  attackDescription: String = "C'est juste une description en vue de mettre en lumière l'attaque. Probablement que chose à avaoir vace l'électricité."; 
+}
